4de6606fb77278a916dae6a8c7c60c373ca49911
khuntiasmriti <khuntia.smriti@gmail.com>
2026-06-18 23:47:23 +0530

    File updated at 6/18/2026, 11:47:22 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.c
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.c
@@ -1,4 +1,4 @@
-// Last updated: 6/18/2026, 11:45:54 PM
+// Last updated: 6/18/2026, 11:47:22 PM
 1#include <stdlib.h>
 2
 3/**
@@ -7,23 +7,19 @@
 6int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
 7    *returnSize = numsSize;
 8
-9    int* answer = (int*)malloc(numsSize * sizeof(int));
-10    if (answer == NULL) {
+9    int* ans = (int*)malloc(numsSize * sizeof(int));
+10    if (ans == NULL) {
 11        return NULL; 
-12    } // <-- FIXED: Added missing closing brace for the if statement
-13
-14    // Forward pass
-15    answer[0] = 1;
-16    for (int i = 1; i < numsSize; i++) {
-17        answer[i] = answer[i - 1] * nums[i - 1];
-18    }
-19    
-20    // Backward pass
-21    int rightProduct = 1;
-22    for (int i = numsSize - 1; i >= 0; i--) {
-23        answer[i] = answer[i] * rightProduct;
-24        rightProduct = rightProduct * nums[i]; 
-25    } // <-- FIXED: Added missing closing brace for the for loop
-26    
-27    return answer;
-28}
+12    } 
+13    ans[0] = 1;
+14    for (int i = 1; i < numsSize; i++) {
+15        ans[i] = ans[i - 1] * nums[i - 1];
+16    }
+17    int rp = 1;
+18    for (int i = numsSize - 1; i >= 0; i--) {
+19        ans[i] = ans[i] * rp;
+20        rp = rp * nums[i]; 
+21    } 
+22    
+23    return ans;
+24}
